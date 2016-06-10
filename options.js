dbd8a9aaabf3e27951ec2e6b37284ce61e1ea17c
Will <will@willthewebmechanic.com>
2016-06-10 06:44:21 -0500

    changes variable name. Begin work on adding language switcher support.

--- options.js
+++ options.js
@@ -17,13 +17,13 @@
  */
 // Saves options to chrome.storage.sync.
 function save_options() {
-	var likesColor = document.getElementById('hide-yoast').checked;
+	var hideYoast = document.getElementById('hide-yoast').checked
 	chrome.storage.sync.set({
-		hideYoast: likesColor
+		hideYoast: hideYoast,
 	}, function() {
 		// Update status to let user know options were saved.
-		var status = document.getElementById('status');
-		status.textContent = 'Options saved.';
+		var status = document.getElementById('status')
+		status.textContent = 'Options saved.'
 		setTimeout(function() {
 			status.textContent = '';
 		}, 750);
@@ -33,11 +33,10 @@ function save_options() {
 // Restores select box and checkbox state using the preferences
 // stored in chrome.storage.
 function restore_options() {
-	// Use default value color = 'red' and likesColor = true.
 	chrome.storage.sync.get({
-		hideYoast: true
+		hideYoast: true,
 	}, function(items) {
-		document.getElementById('hide-yoast').checked = items.hideYoast;
+		document.getElementById('hide-yoast').checked = items.hideYoast
 	});
 }
 document.addEventListener('DOMContentLoaded', restore_options);
